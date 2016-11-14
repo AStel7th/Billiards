386c1e8d4962bbfeab8baf7e1215b3086e35ce96
AStel7th <nkc.keisuke.honda@gmail.com>
2016-11-15 01:39:21 +0900

    [add]衝突判定用コライダークラス追加

--- Billiards/Billiards/Header/Collider.h
+++ Billiards/Billiards/Header/Collider.h
@@ -1,24 +1,59 @@
-//#pragma once
-//#include <unordered_map>
-//#include <vector>
-//
-//using namespace std;
-//
-//class GameObject;
-//
-//class Collider
-//{
-//private:
-//	int spaceID;
-//	GameObject* pObject = nullptr;
-//public:
-//	Collider();
-//	~Collider();
-//
-//	void Create(GameObject* pObj);
-//
-//	int GetSpaceID();
-//};
+#pragma once
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class GameObject;
+
+template<class TYPE = Collider*>
+class _Collider_
+{
+protected:
+	static TYPE pBegin;
+	static TYPE pEnd;
+};
+
+
+template<class TYPE>
+TYPE _Collider_<TYPE>::pBegin = nullptr;
+template<class TYPE>
+TYPE _Collider_<TYPE>::pEnd = nullptr;
+
+
+class Collider : public _Collider_<>
+{
+private:
+	Collider*			pPrev;	//自身の前ポインタ
+	Collider*			pNext;	//自身の後ポインタ
+
+	int spaceID;
+	GameObject* pObject;
+
+	//システムへの登録
+	static inline void _Register_(Collider* pObj);
+
+	//システムから消去　次のポインタが返される
+	static inline Collider* _Unregister_(Collider* pObj);
+	
+public:
+	Collider();
+	virtual ~Collider();
+
+	void Create(GameObject* pObj);
+
+	void Update();
+
+	int GetSpaceID();
+	
+	virtual void Destroy();
+
+	class All
+	{
+	public:
+		static void HitCheck();
+	};
+};
 //
 //class ColliderContainer
 //{
--- Billiards/Billiards/cpp/Collider.cpp
+++ Billiards/Billiards/cpp/Collider.cpp
@@ -1,21 +1,107 @@
-//#include "../Header/Collider.h"
-//
-//Collider::Collider()
-//{
-//
-//}
-//
-//Collider::~Collider()
-//{
-//
-//}
-//
-//void Collider::Create(GameObject* pObj)
-//{
-//
-//}
-//
-//void ColliderContainer::AddCollider(Collider* pCol)
-//{
-//	colliderList[pCol->GetSpaceID()].push_back(pCol);
-//}
+#include "../Header/Collider.h"
+#include "../Header/func.h"
+
+//システムへの登録
+inline void Collider::_Register_(Collider* pCol)
+{
+	//先頭が空の場合は新規タスクを設定
+	if (pBegin == nullptr) pBegin = pCol;
+	else
+	{
+		//新規タスクの前に末尾タスクを代入
+		pCol->pPrev = pEnd;
+
+		//末尾タスクの次に新規タスクを代入
+		pEnd->pNext = pCol;
+	}
+
+	//末尾タスクが新規タスクになる
+	pEnd = pCol;
+}
+
+//システムから消去　次のポインタが返される
+inline Collider* Collider::_Unregister_(Collider* pCol)
+{
+	Collider* next = pCol->pNext; //自身の次
+	Collider* prev = pCol->pPrev; //自身の前
+
+									//次位置に前位置を代入してつなぎ合わせる
+	if (next != nullptr) next->pPrev = prev;
+	//null の場合は末尾タスクがなくなったので、前のタスクを末尾にする
+	else pEnd = prev;
+
+	//前に次を代入してつなぎ合わせる
+	if (prev != nullptr) prev->pNext = next;
+	//null の場合は先頭タスクがなくなったので、次のタスクを先頭にする
+	else pBegin = next;
+
+	//タスクの消去
+	SAFE_DELETE(pCol);
+
+	//次のタスクを返す
+	return next;
+}
+
+Collider::Collider() : pPrev(nullptr), pNext(nullptr), spaceID(0), pObject(nullptr)
+{
+	
+}
+
+Collider::~Collider()
+{
+
+}
+
+void Collider::Create(GameObject * pObj)
+{
+	Collider::_Register_(this);
+	pObject = pObj;
+}
+
+void Collider::Update()
+{
+
+}
+
+int Collider::GetSpaceID()
+{
+	return spaceID;
+}
+
+void Collider::Destroy()
+{
+	Collider::_Unregister_(this);
+}
+
+
+void Collider::All::HitCheck()
+{
+	//Collider* pCol = pBegin; //現在のタスク
+
+	//						   //末尾までループする
+	//while (pCol != nullptr)
+	//{
+	//	//次のオブジェクトを事前取得
+	//	GameObject* _next = pObj->pNext;
+
+	//	//タスクの消去
+	//	switch (pObj->mode)
+	//	{
+	//	case DestroyMode::None:
+	//		pObj->Update();
+	//		break;
+
+	//	case DestroyMode::Destroy:
+	//		//Task::All::Update が呼び出された時に消去
+	//		GameObject::_Unregister_(pObj);
+	//		break;
+
+	//	default:
+	//		assert(!"DestroyMode error");
+	//		break;
+	//	}
+
+	//	//次のタスクへ移動
+	//	pObj = _next;
+	//}
+}
