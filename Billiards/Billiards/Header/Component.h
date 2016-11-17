6d223880b4b04721e3b8322483c8bb4a2cf5c3cf
AStel7th <nkc.keisuke.honda@gmail.com>
2016-11-17 18:37:17 +0900

    [update]PhysicsComponentに要素追加

--- Billiards/Billiards/Header/Component.h
+++ Billiards/Billiards/Header/Component.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <DirectXMath.h>
+#include <typeindex>
+
+using namespace std;
+using namespace DirectX;
+
 class GameObject;
 
 class Component
 {
 public:
+	type_index id;
+	Component() : id(typeid(nullptr)){}
 	virtual ~Component() {}
 	virtual void receive(int message) = 0;
 };
@@ -12,19 +20,27 @@ class InputComponent : public Component
 {
 public:
 	virtual ~InputComponent() {}
-	virtual void Update(GameObject& object) = 0;
+	virtual void Update() = 0;
 };
 
 class PhysicsComponent : public Component
 {
 public:
+	GameObject* pGameObject;
+	XMFLOAT3 prePos;		// 前フレームの位置
+	XMFLOAT3 velocity;		// 移動ベクトル
+	XMFLOAT3 acceleration;	// 加速度
+	float	 mass;			// 質量
+
+	PhysicsComponent(): Component(),prePos(0.0f,0.0f,0.0f),velocity(0.0f, 0.0f, 0.0f),acceleration(0.0f, 0.0f, 0.0f),mass(0.0f){}
+
 	virtual ~PhysicsComponent(){}
-	virtual void Update(GameObject& object) = 0;
+	virtual void Update() = 0;
 };
 
 class GraphicsComponent : public Component
 {
 public:
 	virtual ~GraphicsComponent(){}
-	virtual void Update(GameObject& object) = 0;
+	virtual void Update() = 0;
 };
