b9f32d512cb05f4d240e9f3361272b72741eccbd
AStel7th <nkc.keisuke.honda@gmail.com>
2016-11-27 03:23:12 +0900

    [clean]不要な部分削除

--- Billiards/Billiards/Header/BilliardsTable.h
+++ Billiards/Billiards/Header/BilliardsTable.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "GameObject.h"
 
-class FBXRenderDX11;
-class CollisionFromFBX;
 class BilliardsTablePhysics;
 class MeshCollider;
 class BilliardsTableGraphics;
@@ -15,8 +13,6 @@ private:
 	MeshCollider*				pCollider;
 	BilliardsTablePhysics*		pPhysicsComponent;
 	BilliardsTableGraphics*		pGraphicsComponent;
-	XMFLOAT4X4					m_World;
-	float						ang;
 public:
 	BilliardsTable();
 
--- Billiards/Billiards/cpp/BilliardsTable.cpp
+++ Billiards/Billiards/cpp/BilliardsTable.cpp
@@ -1,56 +1,47 @@
 #include "../Header/BilliardsTable.h"
-#include "../Header/FBXRendererDX11.h"
-#include "../Header/CollisionFromFBX.h"
-#include "../Header/Camera.h"
 #include "../Header/ResourceManager.h"
 #include "../Header/Collider.h"
-#include "../Header/BilliardsTablePhysics.h"
-#include "../Header/BilliardsTableGraphics.h"
+#include "../Header/BilliardsTableComponents.h"
 #include "../Header/MeshData.h"
 
 BilliardsTable::BilliardsTable() : GameObject()
 {
-	/*pTableModel = NEW FBXRenderDX11();
-	pCollisionModel = NEW FBXRenderDX11();*/
-
-	pMesh = ResourceManager::Instance().GetResource("Table", "Resource/billiardsTable_light.fbx");
-	//ResourceManager::Instance().GetResource(*pCollisionModel, "col", "Resource/billiardsTableCollider.fbx");
+	//pMesh = ResourceManager::Instance().GetResource("Table", "Resource/billiardsTableCollider.fbx");
+	pMesh = ResourceManager::Instance().GetResource("Table", "Resource/BilliardsTableModel.fbx");
+	//pMesh = ResourceManager::Instance().GetResource("Table", "Resource/goblin2.fbx");
 
 	pPhysicsComponent = NEW BilliardsTablePhysics(this);
 
 	pGraphicsComponent = NEW BilliardsTableGraphics(this,pMesh);
 	
 	pCollider = NEW MeshCollider();
-	pCollider->Create(this,Mesh,"Resource/billiardsTableCollider.fbx" ,15.0f);
+	pCollider->Create(this,Mesh,"Resource/billiardsTableCollider.fbx" ,180.0f);
 
-	XMStoreFloat4x4(&m_World, XMMatrixIdentity());
+	XMMATRIX wMat = XMMatrixIdentity();
+	wMat *= XMMatrixRotationQuaternion(XMVectorSet(rot.x, rot.y, rot.z, 1.0f));
+	wMat *= XMMatrixTranslation(pos.x, pos.y, pos.z);
 
-	ang = 0.0f;
+	XMStoreFloat4x4(&world, wMat);
 }
 
 BilliardsTable::~BilliardsTable()
 {
-	ResourceManager::Instance().ReduceReference("Table");
 	pCollider->Destroy();
-	/*SAFE_DELETE(pTableModel);
-	SAFE_DELETE(pCollisionModel);*/
 	SAFE_DELETE(pPhysicsComponent);
 	SAFE_DELETE(pGraphicsComponent);
 }
 
 void BilliardsTable::Update()
 {
-	ang = 0.01f;
+	XMMATRIX wMat = XMMatrixIdentity();
+	wMat *= XMMatrixRotationQuaternion(XMVectorSet(rot.x, rot.y, rot.z, 1.0f));
+	wMat *= XMMatrixTranslation(pos.x, pos.y, pos.z);
 
-	XMStoreFloat4x4(&m_World, XMLoadFloat4x4(&m_World)*XMMatrixRotationY(ang));
-
-	/*pTableModel->SetMatrix(m_World, Camera::Instance().view, Camera::Instance().proj);
-
-	pCollisionModel->SetMatrix(m_World, Camera::Instance().view, Camera::Instance().proj);*/
+	XMStoreFloat4x4(&world, wMat);
 
 	pPhysicsComponent->Update();
 
-	pCollider->Update();
-
 	pGraphicsComponent->Update();
+	
+	pCollider->Update();
 }
