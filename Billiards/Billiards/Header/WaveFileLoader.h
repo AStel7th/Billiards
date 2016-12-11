51bcbbc7880acbb7fe3a604ff9c66dd241fe0946
AStel7th <nkc.keisuke.honda@gmail.com>
2016-12-12 02:15:02 +0900

    [add]Waveファイル読み込みクラス

---
+++ Billiards/Billiards/Header/WaveFileLoader.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <Windows.h>
+#include "func.h"
+#include "Exception.h"
+
+#define WAVEFILE_READ   1
+#define WAVEFILE_WRITE  2
+
+class WaveFileLoader
+{
+public:
+	WAVEFORMATEX* m_pwfx;        // Pointer to WAVEFORMATEX structure
+	HMMIO m_hmmio;       // MM I/O handle for the WAVE
+	MMCKINFO m_ck;          // Multimedia RIFF chunk
+	MMCKINFO m_ckRiff;      // Use in opening a WAVE file
+	DWORD m_dwSize;      // The size of the wave file
+	MMIOINFO m_mmioinfoOut;
+	DWORD m_dwFlags;
+	BOOL m_bIsReadingFromMemory;
+	BYTE* m_pbData;
+	BYTE* m_pbDataCur;
+	ULONG m_ulDataSize;
+	CHAR* m_pResourceBuffer;
+protected:
+	HRESULT ReadMMIO();
+	HRESULT WriteMMIO(WAVEFORMATEX* pwfxDest);
+
+public:
+	WaveFileLoader();
+	~WaveFileLoader();
+
+	HRESULT Open(LPWSTR strFileName, WAVEFORMATEX* pwfx, DWORD dwFlags);
+	HRESULT OpenFromMemory(BYTE* pbData, ULONG ulDataSize, WAVEFORMATEX* pwfx, DWORD dwFlags);
+	HRESULT Close();
+
+	HRESULT Read(BYTE* pBuffer, DWORD dwSizeToRead, DWORD* pdwSizeRead);
+	HRESULT Write(UINT nSizeToWrite, BYTE* pbData, UINT* pnSizeWrote);
+
+	DWORD   GetSize();
+	HRESULT ResetFile();
+	WAVEFORMATEX* GetFormat()
+	{
+		return m_pwfx;
+	};
+
+	CHAR* GetData()
+	{
+		return m_pResourceBuffer;
+	}
+
+};
